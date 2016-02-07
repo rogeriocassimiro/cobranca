ab016127daf965fd24dc45c8a7351303f55333a5
rogeriocassimiro <rogerioinhumas@gmail.com>
2016-02-07 11:57:13 -0200

    edicao e exclusao parcial

--- src/main/java/com/cassimiro/controller/TituloController.java
+++ src/main/java/com/cassimiro/controller/TituloController.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cassimiro.enumerado.EnumStatusTitulo;
 import com.cassimiro.model.Titulo;
@@ -19,33 +22,50 @@ import com.cassimiro.repository.TituloRepository;
 @RequestMapping("/rest/titulo")
 public class TituloController {
 
+	private static final String REST_LIST = "/titulo/titulo";
+	public static final String REST_EDIT = "/titulo/tituloEdit";
+	public static final String REST_REDIRECT_NEW = "redirect:/rest/titulo/novo";
+	public static final String REST_REDIRECT_LIST = "redirect:/rest/titulo";
+	
 	@Autowired
 	private TituloRepository tituloRepository;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(){
-		ModelAndView mv = new ModelAndView("/titulo/tituloEdit");
+		ModelAndView mv = new ModelAndView(REST_EDIT);
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(value = "/salvar", method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo){
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes){
+		if(errors.hasErrors()){
+			return REST_EDIT;
+		}
 		tituloRepository.save(titulo);
-		ModelAndView mv = new ModelAndView("/titulo/tituloEdit");
-		mv.addObject("mensagem", "Registro salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Registro salvo com sucesso!");
+		return REST_REDIRECT_NEW;
 	}
 	
 	@RequestMapping
 	public ModelAndView listar(){
-		ModelAndView mv = new ModelAndView("/titulo/titulo");
+		ModelAndView mv = new ModelAndView(REST_LIST);
 		mv.addObject("titulos", tituloRepository.findAll());
 		return mv;
 	}
 	
-	@RequestMapping(value = "/testar")
-	public void testar(@RequestParam String testar){
-		System.out.println(testar);
+	@RequestMapping("/editar/{codigo}")
+	public ModelAndView editar(@PathVariable("codigo") Titulo titulo){
+		ModelAndView mv = new ModelAndView(REST_EDIT);
+		mv.addObject("titulo", titulo);
+		return mv;
+	}
+	
+	@RequestMapping("/remover/{codigo}")
+	public String remover(@PathVariable("codigo") Titulo titulo, RedirectAttributes attributes){
+		this.tituloRepository.delete(titulo);
+		attributes.addFlashAttribute("mensagem", "Registro excluído com sucesso!");
+		return REST_REDIRECT_LIST;
 	}
 	
 	@ModelAttribute("listaStatusTitulo")
--- src/main/java/com/cassimiro/model/Titulo.java
+++ src/main/java/com/cassimiro/model/Titulo.java
@@ -11,7 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -24,12 +29,18 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
+	@NotEmpty(message = "Descrição é obrigatória")
+	@Size(max = 60, message = "A descrição não pode ser maior que 60 caracteres")
 	private String descricao;
 
+	@NotNull(message = "Data de vencimento é obrigatório")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
 
+	@NotNull( message = "Valor é obrigatório")
+	@DecimalMin(value = "0.01", message	= "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999999.99", message = "Valor não pode ser maior que 9.999.999.999,99")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 
